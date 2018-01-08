bb478ee745bd380f9f4f709d9d908275daea8747
hieunguyen <hieu.nguyen@xnprotel.com>
2018-01-08 17:42:44 +0700

    Add loading mask; Change text and style

--- Sourcecode/GenericPayment/Assets/js/nl-checkout.js
+++ Sourcecode/GenericPayment/Assets/js/nl-checkout.js
@@ -1,4 +1,39 @@
-﻿jQuery("#agree").click(function (e) {
+﻿$(document).ready(function () {
+    var spinner = new Spinner({
+        lines: 11, // The number of lines to draw
+        length: 33, // The length of each line
+        width: 9, // The line thickness
+        radius: 45, // The radius of the inner circle
+        scale: 1, // Scales overall size of the spinner
+        corners: 1, // Corner roundness (0..1)
+        color: '#ffff80', // CSS color or array of colors
+        fadeColor: 'transparent', // CSS color or array of colors
+        opacity: 0.25, // Opacity of the lines
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        fps: 20, // Frames per second when using setTimeout() as a fallback in IE 9
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        className: 'spinner', // The CSS class to assign to the spinner
+        top: '50%', // Top position relative to parent
+        left: '50%', // Left position relative to parent
+        shadow: 'none', // Box-shadow for the lines
+        position: 'absolute' // Element positioning
+    }).spin(document.getElementById("loadingMask")); 
+
+    $(document).bind("ajaxSend", function () {
+        $("#loadingMask").show();
+    }).bind("ajaxComplete", function () {
+        $("#loadingMask").fadeOut();
+    }).ajaxError(function (event, response, settings, thrownError) {
+        $("#loadingMask").fadeOut();
+    });
+
+    $("#loadingMask").fadeOut();
+});
+
+jQuery("#agree").click(function (e) {
     e.preventDefault();
     var data = {};
     data["CashKey"] = $("#cashkey").val();
